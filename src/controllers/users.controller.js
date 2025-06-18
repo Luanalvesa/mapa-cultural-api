7ad1746e32fe0a5e804eb6080facd033830b4165
Luana Alves <luana.alvesqa@gmail.com>
2025-06-18 18:50:16 -0300

    refactor (controllers users.controller): Adjust methos to auth user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,47 +7,45 @@ const service = new UserService(userRepo);
 
 module.exports = {
   register: async (request, response) => {
-    const { name, email, password } = request.body;
-
-    if (!name || name.trim() === "") {
-      return response.status(400).json({ error: "O campo 'name' é obrigatório." });
-    }
-    if (!email || email.trim() === "") {
-      return response.status(400).json({ error: "O campo 'email' é obrigatório." });
-    }
-
     try {
+      const { name, userName, email, password } = request.body;
+
+      const existUser = await service.findOneByEmail(email) || await service.findOneByUserName(userName)
+
+      if (existUser) return response.status(400).json({ message: 'Exist user' })
+
       const encrypted = CryptoJS.SHA256(password).toString();
-      const user = await service.create({ name, email, password: encrypted });
+      const user = await service.create({ name, email, userName, password: encrypted });
 
       response
         .status(201)
-        .json({ id: user.id, name: user.name, email: user.email });
-    } catch (err) {
-      response.status(400).json({ error: err.message });
+        .json({
+          id: user.id,
+          name: user.name,
+          userName: user.userName,
+          email: user.email
+        });
+    } catch (error) {
+      response.status(500).json({ error: error.message });
     }
   },
-
   login: async (request, response) => {
-    const { email, password } = request.body;
+    try {
+      const { email, password } = request.body;
 
-    if (!email || email.trim() === "") {
-      return response.status(400).json({ error: "O campo 'email' é obrigatório." });
+      const encrypted = CryptoJS.SHA256(password).toString();
+
+      const user = await service.findOneBy(email);
+
+      if (!user) return response.status(400).json({ message: "User not found" });
+
+      if (user.password !== encrypted) return response.status(400).json({ message: "Password not match" });
+
+      response.json({
+        user: { id: user.id, name: user.name, email: user.email },
+      });
+    } catch (error) {
+      response.status(500).json({ error: error.message });
     }
-    if (!password || password.trim() === "") {
-      return response.status(400).json({ error: "O campo 'password' é obrigatório." });
-    }
-
-    const encrypted = CryptoJS.SHA256(password).toString();
-
-    const user = await service.findOneBy({ email, password: encrypted });
-
-    if (!user)
-      return response.status(401).json({ message: "Invalid credentials" });
-
-
-    response.json({
-      user: { id: user.id, name: user.name, email: user.email },
-    });
   },
 };
