579b415a579374e0d67638fa84770be568fecd74
Luana Alves <luana.alvesqa@gmail.com>
2025-06-18 19:00:44 -0300

    refactor (services places): Adjust events service

--- src/services/places.service.js
+++ src/services/places.service.js
@@ -1,61 +1,15 @@
-const Place = require('../entities/Place'); 
-
 class PlaceService {
-  async createPlace(placeData) {
-    try {
-      const newPlace = await Place.create(placeData);
-      return newPlace;
-    } catch (error) {
-      console.error('Erro ao criar lugar:', error);
-      throw new Error('Falha ao criar lugar.');
-    }
+  constructor(repo) {
+    this.repo = repo;
   }
 
-  async getAllPlaces() {
-    try {
-      const places = await Place.findAll(); 
-      return places;
-    } catch (error) {
-      console.error('Erro ao buscar lugares:', error);
-      throw new Error('Falha ao buscar lugares.');
-    }
+  async create(data) {
+    return await this.repo.save(data);
   }
 
-  async getPlaceById(id) {
-    try {
-      const place = await Place.findByPk(id); 
-      return place;
-    } catch (error) {
-      console.error(`Erro ao buscar lugar com ID ${id}:`, error);
-      throw new Error(`Falha ao buscar lugar.`);
-    }
-  }
-
-  async updatePlace(id, updatedData) {
-    try {
-      const place = await Place.findByPk(id);
-      if (!place) {
-        return null; 
-      }
-      await place.update(updatedData);
-      return place;
-    } catch (error) {
-      console.error(`Erro ao atualizar lugar com ID ${id}:`, error);
-      throw new Error('Falha ao atualizar lugar.');
-    }
-  }
-
-  async deletePlace(id) {
-    try {
-      const result = await Place.destroy({
-        where: { id: id },
-      });
-      return result; 
-    } catch (error) {
-      console.error(`Erro ao deletar lugar com ID ${id}:`, error);
-      throw new Error('Falha ao deletar lugar.');
-    }
+  async list() {
+    return await this.repo.find();
   }
 }
 
-module.exports = new PlaceService();
+module.exports = PlaceService;
