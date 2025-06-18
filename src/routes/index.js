af5c61e3544e5a775e6f63bec759ea5f9adb9cde
Luana Alves <luana.alvesqa@gmail.com>
2025-06-18 19:01:00 -0300

    fix (routes): Adjust routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,16 +8,10 @@ const eventController = require('../controllers/events.controller');
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 
-router.post('/places', placeController.create); 
-router.get('/places', placeController.getAll);   
-router.get('/places/:id', placeController.getById); 
-router.put('/places/:id', placeController.update); 
-router.delete('/places/:id', placeController.delete); 
+router.post('/places', placeController.createPlace); 
+router.get('/places', placeController.listPlaces);   
 
-router.post('/events', eventController.create); 
-router.get('/events', eventController.getAll);   
-router.get('/events/:id', eventController.getById); 
-router.put('/events/:id', eventController.update); 
-router.delete('/events/:id', eventController.delete); 
+router.post('/events', eventController.createEvent); 
+router.get('/events', eventController.listEvents);   
 
 module.exports = router;
